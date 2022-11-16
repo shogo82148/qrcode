8fe208d9bf2e13762e45bbe07c0b42ecbcc57e57
ICHINOSE Shogo <shogo82148@gmail.com>
2022-11-16 23:30:05 +0900

    encoding number

--- qrcode.go
+++ qrcode.go
@@ -3,6 +3,8 @@ package qrcode
 //go:generate go run genbch/main.go
 
 import (
+	"errors"
+	"fmt"
 	"image"
 
 	"github.com/shogo82148/qrcode/internal/bitstream"
@@ -163,4 +165,111 @@ func Generate() image.Image {
 		}
 	}
 	return img
+}
+
+type Version int
+
+type Mode uint8
+
+const (
+	// ModeECI is ECI(Extended Channel Interpretation) mode.
+	ModeECI Mode = 0b0111
+
+	// ModeNumber is number mode.
+	// The Data must be ascii characters [0-9].
+	ModeNumber Mode = 0b0001
+
+	// ModeNumber is alphabet and number mode.
+	// The Data must be ascii characters [0-9A-Z $%*+\-./:].
+	ModeAlphabet Mode = 0b0010
+
+	// ModeBytes is 8-bit bytes mode.
+	// The Data can include any bytes.
+	ModeBytes Mode = 0b0100
+
+	// ModeKanji is Japanese Kanji mode.
+	ModeKanji Mode = 0b1000
+
+	// ModeConnected is connected structure mode.
+	ModeConnected Mode = 0b0011
+
+	ModeFNC1_1 Mode = 0b0101
+	ModeFNC1_2 Mode = 0b1001
+
+	ModeTerminated Mode = 0b0000
+)
+
+type Segment struct {
+	Mode Mode
+	Data []byte
+}
+
+func (s *Segment) encode(version Version, buf *bitstream.Buffer) error {
+	switch s.Mode {
+	case ModeNumber:
+		return s.encodeNumber(version, buf)
+	default:
+		return errors.New("qrcode: unknown mode")
+	}
+}
+
+func (s *Segment) encodeNumber(version Version, buf *bitstream.Buffer) error {
+	// validation
+	var n int
+	data := s.Data
+	switch {
+	case version <= 0 || version > 40:
+		return fmt.Errorf("qrcode: invalid version: %d", version)
+	case version < 10:
+		n = 10
+	case version < 27:
+		n = 12
+	default:
+		n = 14
+	}
+	if len(data) >= 1<<n {
+		return fmt.Errorf("qrcode: data is too long: %d", len(data))
+	}
+
+	for _, ch := range data {
+		if ch < '0' || ch > '9' {
+			return fmt.Errorf("qrcode: invalid character in number mode: %02x", ch)
+		}
+	}
+
+	// mode
+	buf.WriteBitsLSB(uint64(ModeNumber), 4)
+
+	// data length
+	switch {
+	case version <= 0 || version > 40:
+		return fmt.Errorf("qrcode: invalid version: %d", version)
+	case version < 10:
+		buf.WriteBitsLSB(uint64(len(s.Data)), 10)
+	case version < 27:
+		buf.WriteBitsLSB(uint64(len(s.Data)), 12)
+	default:
+		buf.WriteBitsLSB(uint64(len(s.Data)), 14)
+	}
+
+	// data
+	for i := 0; i+2 < len(data); i += 3 {
+		n1 := int(data[i+0] - '0')
+		n2 := int(data[i+1] - '0')
+		n3 := int(data[i+2] - '0')
+		bits := n1*100 + n2*10 + n3
+		buf.WriteBitsLSB(uint64(bits), 10)
+	}
+
+	switch len(data) % 3 {
+	case 1:
+		bits := data[len(data)-1] - '0'
+		buf.WriteBitsLSB(uint64(bits), 4)
+	case 2:
+		n1 := int(data[len(data)-2] - '0')
+		n2 := int(data[len(data)-1] - '0')
+		bits := n1*10 + n2
+		buf.WriteBitsLSB(uint64(bits), 7)
+	}
+	return nil
 }
--- qrcode_test.go
+++ qrcode_test.go
@@ -5,6 +5,8 @@ import (
 	"image/png"
 	"os"
 	"testing"
+
+	"github.com/shogo82148/qrcode/internal/bitstream"
 )
 
 func TestQRCode(t *testing.T) {
@@ -15,5 +17,26 @@ func TestQRCode(t *testing.T) {
 	}
 	if err := os.WriteFile("qrcode.png", buf.Bytes(), 0o644); err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestSegment_encodeNumber(t *testing.T) {
+	s := &Segment{
+		Mode: ModeNumber,
+		Data: []byte("01234567"),
+	}
+	var buf bitstream.Buffer
+
+	if err := s.encode(1, &buf); err != nil {
+		t.Fatal(err)
+	}
+
+	want := []byte{
+		0b0001_0000, 0b001000_00, 0b00001100, 0b01010110, 0b01_100001, 0b1_0000000,
+	}
+	got := buf.Bytes()
+
+	if !bytes.Equal(got, want) {
+		t.Errorf("got %08b, want %08b", got, want)
 	}
 }
