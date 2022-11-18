a38d3b5d6d369bf8e0182667bdf93cabe56031ea
ICHINOSE Shogo <shogo82148@gmail.com>
2022-11-18 21:13:08 +0900

    fix test of SetBinary

--- internal/binimage/binary_test.go
+++ internal/binimage/binary_test.go
@@ -7,12 +7,12 @@ import (
 
 func TestSetBinary(t *testing.T) {
 	img := New(image.Rect(0, 0, 8, 8))
-	img.SetBinary(0, 0, White)
+	img.SetBinary(0, 0, Black)
 
 	if img.Pix[0] != 0x80 {
 		t.Errorf("got %02x, want %02x", img.Pix[0], 0x80)
 	}
-	if img.BinaryAt(0, 0) != White {
-		t.Errorf("got %v, want %v", img.BinaryAt(0, 0), White)
+	if img.BinaryAt(0, 0) != Black {
+		t.Errorf("got %v, want %v", img.BinaryAt(0, 0), Black)
 	}
 }
