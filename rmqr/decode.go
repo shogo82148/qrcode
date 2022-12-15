cf89169ff386dc6f052d56cedff32af72f6e0dd0
ICHINOSE Shogo <shogo82148@gmail.com>
2022-12-15 21:50:29 +0900

    add more samples

--- rmqr/decode.go
+++ rmqr/decode.go
@@ -13,22 +13,44 @@ import (
 
 func DecodeBitmap(img *bitmap.Image) (*QRCode, error) {
 	binimg := internalbitmap.Import(img)
+	bounds := img.Bounds()
+	w := bounds.Dx() - 1
+	h := bounds.Dy() - 1
 
 	var rawVersion uint
+
+	for i := 0; i < 15; i++ {
+		if binimg.BinaryAt(w-7+i/5, h-5+i%5) {
+			rawVersion |= 1 << i
+		}
+	}
+	if binimg.BinaryAt(w-4, h-5) {
+		rawVersion |= 1 << 15
+	}
+	if binimg.BinaryAt(w-3, h-5) {
+		rawVersion |= 1 << 16
+	}
+	if binimg.BinaryAt(w-2, h-5) {
+		rawVersion |= 1 << 17
+	}
+	version, level, ok := decodeFormat(rawVersion ^ 0b100000101001111011)
+	if !ok {
+		// return nil, errors.New("rmqr: rMQR not found")
+	}
+	log.Println(version, level)
+
+	rawVersion = 0
 	for i := 0; i < 18; i++ {
 		if binimg.BinaryAt(8+i/5, 1+i%5) {
 			rawVersion |= 1 << i
 		}
 	}
-	version, _, ok := decodeFormat(rawVersion ^ 0b011111101010110010)
+	version, level, ok = decodeFormat(rawVersion ^ 0b011111101010110010)
 	if !ok {
 		return nil, errors.New("rmqr: rMQR not found")
 	}
+	log.Println(version, level)
 	// TODO: check around sub-finder pattern
-
-	bounds := img.Bounds()
-	w := bounds.Dx() - 1
-	h := bounds.Dy() - 1
 
 	used := usedList[version]
 	binimg.Mask(binimg, used, precomputedMask)
@@ -92,7 +114,7 @@ func DecodeBitmap(img *bitmap.Image) (*QRCode, error) {
 func decodeFormat(data uint) (Version, Level, bool) {
 	var version, min int
 	min = bits.OnesCount(encodedVersion[0] ^ data)
-	for i, v := range encodedVersion[1:] {
+	for i, v := range encodedVersion {
 		diff := bits.OnesCount(v ^ data)
 		if diff < min {
 			version = i
@@ -102,5 +124,6 @@ func decodeFormat(data uint) (Version, Level, bool) {
 	if min >= 3 {
 		return 0, 0, false
 	}
-	return Version(version & 0x1f), Level((version >> 5) & 1), true
+	log.Printf("min: %d, 0b%b", min, version)
+	return Version(version & 0x1f), Level((version >> 6) & 1), true
 }
--- rmqr/decode_test.go
+++ rmqr/decode_test.go
@@ -1,7 +1,6 @@
 package rmqr
 
 import (
-	"fmt"
 	"image"
 	"image/png"
 	"os"
@@ -27,13 +26,7 @@ func TestDecode1(t *testing.T) {
 			X := float64(x)*(164/59.0) + 9
 			Y := float64(y)*(46/15.0) + 9
 			binimg.Set(x, y, img.At(round(X), round(Y)))
-			if binimg.BinaryAt(x, y) {
-				fmt.Print("*")
-			} else {
-				fmt.Print(" ")
-			}
 		}
-		fmt.Println()
 	}
 }
 
@@ -53,6 +46,256 @@ func TestDecode2(t *testing.T) {
 		for x := 0; x < 43; x++ {
 			X := float64(x)*(335/43.0) + 29
 			Y := float64(y)*(50/7.0) + 30
+			binimg.Set(x, y, img.At(round(X), round(Y)))
+		}
+	}
+	_, err = DecodeBitmap(binimg)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestDecode3(t *testing.T) {
+	// from https://www.qrcode.com/img/rmqr/graph.jpg
+	r, err := os.Open("testdata/r7x139.png")
+	if err != nil {
+		t.Fatal(err)
+	}
+	img, err := png.Decode(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	binimg := bitmap.New(image.Rect(0, 0, 139, 7))
+	for y := 0; y < 7; y++ {
+		for x := 0; x < 139; x++ {
+			X := float64(x)*(386/139.0) + 5
+			Y := float64(y)*(20/7.0) + 7
+			binimg.Set(x, y, img.At(round(X), round(Y)))
+		}
+	}
+	_, err = DecodeBitmap(binimg)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestDecode4(t *testing.T) {
+	// from https://www.qrcode.com/img/rmqr/graph.jpg
+	r, err := os.Open("testdata/r9x43.png")
+	if err != nil {
+		t.Fatal(err)
+	}
+	img, err := png.Decode(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	binimg := bitmap.New(image.Rect(0, 0, 43, 9))
+	for y := 0; y < 9; y++ {
+		for x := 0; x < 43; x++ {
+			X := float64(x)*(120/43.0) + 7
+			Y := float64(y)*(25/9.0) + 7
+			binimg.Set(x, y, img.At(round(X), round(Y)))
+		}
+	}
+	_, err = DecodeBitmap(binimg)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestDecode5(t *testing.T) {
+	// from https://www.qrcode.com/img/rmqr/graph.jpg
+	r, err := os.Open("testdata/r9x139.png")
+	if err != nil {
+		t.Fatal(err)
+	}
+	img, err := png.Decode(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	binimg := bitmap.New(image.Rect(0, 0, 139, 9))
+	for y := 0; y < 9; y++ {
+		for x := 0; x < 139; x++ {
+			X := float64(x)*(385.5/139.0) + 7
+			Y := float64(y)*(25/9.0) + 7
+			binimg.Set(x, y, img.At(round(X), round(Y)))
+		}
+	}
+	_, err = DecodeBitmap(binimg)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestDecode6(t *testing.T) {
+	// from https://www.qrcode.com/img/rmqr/graph.jpg
+	r, err := os.Open("testdata/r11x27.png")
+	if err != nil {
+		t.Fatal(err)
+	}
+	img, err := png.Decode(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	binimg := bitmap.New(image.Rect(0, 0, 27, 11))
+	for y := 0; y < 11; y++ {
+		for x := 0; x < 27; x++ {
+			X := float64(x)*(75/27.0) + 6
+			Y := float64(y)*(30/11.0) + 7
+			binimg.Set(x, y, img.At(round(X), round(Y)))
+		}
+	}
+	_, err = DecodeBitmap(binimg)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestDecode7(t *testing.T) {
+	// from https://www.qrcode.com/img/rmqr/graph.jpg
+	r, err := os.Open("testdata/r11x139.png")
+	if err != nil {
+		t.Fatal(err)
+	}
+	img, err := png.Decode(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	binimg := bitmap.New(image.Rect(0, 0, 139, 11))
+	for y := 0; y < 11; y++ {
+		for x := 0; x < 139; x++ {
+			X := float64(x)*(75/27.0) + 7
+			Y := float64(y)*(30/11.0) + 7
+			binimg.Set(x, y, img.At(round(X), round(Y)))
+		}
+	}
+	_, err = DecodeBitmap(binimg)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestDecode8(t *testing.T) {
+	// from https://www.qrcode.com/img/rmqr/graph.jpg
+	r, err := os.Open("testdata/r13x27.png")
+	if err != nil {
+		t.Fatal(err)
+	}
+	img, err := png.Decode(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	binimg := bitmap.New(image.Rect(0, 0, 27, 13))
+	for y := 0; y < 13; y++ {
+		for x := 0; x < 27; x++ {
+			X := float64(x)*(74/27.0) + 9
+			Y := float64(y)*(35/13.0) + 9
+			binimg.Set(x, y, img.At(round(X), round(Y)))
+		}
+	}
+	_, err = DecodeBitmap(binimg)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestDecode9(t *testing.T) {
+	// from https://www.qrcode.com/img/rmqr/graph.jpg
+	r, err := os.Open("testdata/r15x43.png")
+	if err != nil {
+		t.Fatal(err)
+	}
+	img, err := png.Decode(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	binimg := bitmap.New(image.Rect(0, 0, 43, 15))
+	for y := 0; y < 15; y++ {
+		for x := 0; x < 43; x++ {
+			X := float64(x)*(120/43.0) + 9
+			Y := float64(y)*(40/15.0) + 9
+			binimg.Set(x, y, img.At(round(X), round(Y)))
+		}
+	}
+	_, err = DecodeBitmap(binimg)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestDecode10(t *testing.T) {
+	// from https://www.qrcode.com/img/rmqr/graph.jpg
+	r, err := os.Open("testdata/r15x139.png")
+	if err != nil {
+		t.Fatal(err)
+	}
+	img, err := png.Decode(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	binimg := bitmap.New(image.Rect(0, 0, 139, 15))
+	for y := 0; y < 15; y++ {
+		for x := 0; x < 139; x++ {
+			X := float64(x)*(390/139.0) + 7
+			Y := float64(y)*(40/15.0) + 8
+			binimg.Set(x, y, img.At(round(X), round(Y)))
+		}
+	}
+	_, err = DecodeBitmap(binimg)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestDecode11(t *testing.T) {
+	// from https://www.qrcode.com/img/rmqr/graph.jpg
+	r, err := os.Open("testdata/r17x43.png")
+	if err != nil {
+		t.Fatal(err)
+	}
+	img, err := png.Decode(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	binimg := bitmap.New(image.Rect(0, 0, 43, 17))
+	for y := 0; y < 17; y++ {
+		for x := 0; x < 43; x++ {
+			X := float64(x)*(120/43.0) + 7
+			Y := float64(y)*(46/17.0) + 8
+			binimg.Set(x, y, img.At(round(X), round(Y)))
+		}
+	}
+	_, err = DecodeBitmap(binimg)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestDecode12(t *testing.T) {
+	// from https://www.qrcode.com/img/rmqr/graph.jpg
+	r, err := os.Open("testdata/r17x139.png")
+	if err != nil {
+		t.Fatal(err)
+	}
+	img, err := png.Decode(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	binimg := bitmap.New(image.Rect(0, 0, 139, 17))
+	for y := 0; y < 17; y++ {
+		for x := 0; x < 139; x++ {
+			X := float64(x)*(386/139.0) + 7
+			Y := float64(y)*(45/17.0) + 9
 			binimg.Set(x, y, img.At(round(X), round(Y)))
 		}
 	}
