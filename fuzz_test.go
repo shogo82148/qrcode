c5d67dc40247265c774360459336920bd1b5bb8c
ICHINOSE Shogo <shogo82148@gmail.com>
2023-09-17 00:30:09 +0900

    fix

--- fuzz_test.go
+++ fuzz_test.go
@@ -73,7 +73,7 @@ func FuzzNewFromKanji(f *testing.F) {
 		// encode and decode
 		img, err := qr0.EncodeToBitmap()
 		if err != nil {
-			t.Fatal(err)
+			return
 		}
 		_, err = DecodeBitmap(img)
 		if err != nil {
